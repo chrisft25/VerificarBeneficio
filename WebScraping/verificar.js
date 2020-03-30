166800c3259b92d12c0f04b6bd5bc3608d56ee6b
belcboo <me@belcboo.com>
2020-03-30 16:21:45 -0400

    update to URL

--- WebScraping/verificar.js
+++ WebScraping/verificar.js
@@ -7,11 +7,14 @@ const API_URL = process.env.API_URL;
 
 app.get("/sms/:dui", async (req, res) => {
   let dui = req.params.dui;
-  let response = { success: false, msg: "El número de DUI ingresado es inválido." };
+  let response = {
+    success: false,
+    msg: "El número de DUI ingresado es inválido."
+  };
   if (dui.length === 9) {
     dui = dui.substr(0, 8) + "-" + dui.substr(8, 1);
     if (validarDUI(dui)) {
-      response = await buscarInfo(dui,1);
+      response = await buscarInfo(dui, 1);
     }
   }
   res.json(response);
@@ -23,7 +26,7 @@ app.get("/call", async (req, res) => {
   if (dui.length === 9) {
     dui = dui.substr(0, 8) + "-" + dui.substr(8, 1);
     if (validarDUI(dui)) {
-      response = await buscarInfo(dui,2);
+      response = await buscarInfo(dui, 2);
     }
   }
   res.send(response);
@@ -33,10 +36,12 @@ app.listen(PORT, () => {
   console.log(`App Running in port ${PORT}`);
 });
 
-const buscarInfo = async (dui,tipo) => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+const buscarInfo = async (dui, tipo) => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
   const page = await browser.newPage();
-  await page.goto(API_URL);
+  await page.goto("https://emergencia.covid19.gob.sv/");
   //await page.waitForSelector("#cf-error-details");
   //await page.click('button[type="submit"]')
   await page.waitForSelector("#dui");
@@ -52,33 +57,34 @@ const buscarInfo = async (dui,tipo) => {
   await page.waitForSelector("#dui");
   const respuesta = await page.evaluate(() => {
     const elemento = document.querySelector("#accepted");
-    return elemento ? elemento.textContent.replace(/\n/g, " ").trim() : false;
+    return elemento
+      ? elemento.textContent.replace(/\n/g, " ").trim()
+      : false;
   });
   await browser.close();
   let response;
   if (respuesta) {
-    switch(tipo){
+    switch (tipo) {
       case 1:
         response = { success: true, msg: respuesta };
         break;
-      
+
       case 2:
-        response= `<Response><Say language="es" voice="woman">${respuesta}</Say></Response>`;
+        response = `<Response><Say language="es" voice="woman">${respuesta}</Say></Response>`;
         break;
     }
-    
   } else {
-      switch(tipo){
+    switch (tipo) {
       case 1:
         response = {
-      success: false,
-      msg:
-        "Lo sentimos, este DUI no está sujeto a recibir el beneficio de los $300 dólares. Intenta ingresando el DUI de otra persona de tu vivienda. Si después de haber consultado todos los números de DUI de tu grupo familiar y ninguno aparece en el registro, dirígete al Centro de Atención por Demanda (CENADE) más cercano."
-    };
+          success: false,
+          msg:
+            "Lo sentimos, este DUI no está sujeto a recibir el beneficio de los $300 dólares. Intenta ingresando el DUI de otra persona de tu vivienda. Si después de haber consultado todos los números de DUI de tu grupo familiar y ninguno aparece en el registro, dirígete al Centro de Atención por Demanda (CENADE) más cercano."
+        };
         break;
-      
+
       case 2:
-        response= `<Response><Say language="es" voice="woman">Lo sentimos, este DUI no está sujeto a recibir el beneficio de los $300 dólares. Intenta ingresando el DUI de otra persona de tu vivienda. Si después de haber consultado todos los números de DUI de tu grupo familiar y ninguno aparece en el registro, dirígete al Centro de Atención por Demanda (CENADE) más cercano.</Say></Response>`;
+        response = `<Response><Say language="es" voice="woman">Lo sentimos, este DUI no está sujeto a recibir el beneficio de los $300 dólares. Intenta ingresando el DUI de otra persona de tu vivienda. Si después de haber consultado todos los números de DUI de tu grupo familiar y ninguno aparece en el registro, dirígete al Centro de Atención por Demanda (CENADE) más cercano.</Say></Response>`;
         break;
     }
   }
